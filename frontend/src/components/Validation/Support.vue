25def7ad46c427181c759a3349d8e253f8e565dc
Jaclyn Chan <jaclyn.chan@curvegrid.com>
2021-02-01 15:54:32 +0800

    add validator to front end

--- frontend/src/components/Validation/Support.vue
+++ frontend/src/components/Validation/Support.vue
@@ -11,6 +11,36 @@
           Bitfinca is built on the Ethereum blockchain and smart contracts, making sure that it is transparent, efficient, and secure. Join our decentralized network of validators who stake some amount of tokens, earn a fee for it, and provide documents to a trusted third party.</v-card-text>
       </v-card>
     </v-container>
+     <v-container>
+      <v-card>
+        <v-card-title class="page-header-one" style="font-size: 32px;">Validator Application</v-card-title>
+        <v-card-text class="body-text">
+          <v-form>
+            <v-row>
+              <v-col cols="12"
+              md="6">
+              <v-text-field
+                v-model="firstname"
+                :rules="nameRules"
+                label="Applicant name"
+                required
+              ></v-text-field>
+              </v-col>
+               <v-col cols="12"
+              md="5">
+              <v-text-field
+                v-model="firstname"
+                :rules="nameRules"
+                label="Wallet Address"
+                required
+              ></v-text-field>
+              </v-col>
+              <v-spacer/><v-btn class="mt-10 ma-4 page-button">Submit</v-btn>
+            </v-row>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-container>
   </div>
 </template>
 
