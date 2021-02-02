eafe98a209c2130774886d251bf337ebb8114427
Jaclyn Chan <jaclyn.chan@curvegrid.com>
2021-02-02 14:43:57 +0800

    make account contract ERC20

--- contracts/contracts/Account.sol
+++ contracts/contracts/Account.sol
@@ -1,61 +1,242 @@
 pragma solidity >=0.4.22 <0.9.0;
+import "./IERC20.sol";
 
 contract Account {
+  mapping (address => uint256) private _balances;
+  mapping (address => mapping (address => uint256)) private _allowed;
+
+  uint256 private _totalSupply;
 
   address private owner;
-
-  event Send(address from, address to, uint256 amount);
-  event Deposit(uint256 amount);
-  event Withdraw(uint256 amount);
-
-  mapping(address => uint256) balances; // account -> balance
 
   constructor() public {
     owner = msg.sender;
   }
 
-  function send(address _to, uint256 _amount) public {
-      require(balances[msg.sender] >= _amount, "insufficient funds");
-      balances[msg.sender] -= _amount;
-      balances[_to] += _amount;
+  event Transfer(
+    address indexed from,
+    address indexed to,
+    uint256 value
+  );
 
-      emit Send(msg.sender, _to, _amount);
+  event Approval(
+    address indexed owner,
+    address indexed spender,
+    uint256 value
+  );
+
+  event Deposit(uint256 amount);
+  event Withdraw(uint256 amount);
+
+  /**
+  * @dev Total number of tokens in existence
+  */
+  function totalSupply() public view returns (uint256) {
+    return _totalSupply;
+  }
+
+  /**
+  * @dev Gets the balance of the specified address.
+  * @param _account The address to query the balance of.
+  * @return An uint256 representing the amount owned by the passed address.
+  */
+  function balanceOf(address _account) public view returns (uint256) {
+    return _balances[_account];
+  }
+
+  /**
+   * @dev Function to check the amount of tokens that an owner allowed to a spender.
+   * @param _owner address The address which owns the funds.
+   * @param spender address The address which will spend the funds.
+   * @return A uint256 specifying the amount of tokens still available for the spender.
+   */
+  function allowance(
+    address _owner,
+    address spender
+   )
+    public
+    view
+    returns (uint256)
+  {
+    return _allowed[_owner][spender];
+  }
+
+  /**
+  * @dev Transfer token for a specified address
+  * @param to The address to transfer to.
+  * @param value The amount to be transferred.
+  */
+  function transfer(address to, uint256 value) public returns (bool) {
+    require(value <= _balances[msg.sender], "insufficient funds");
+    require(to != address(0));
+
+    _balances[msg.sender] = _balances[msg.sender] - value;
+    _balances[to] = _balances[to] + value;
+    emit Transfer(msg.sender, to, value);
+    return true;
+  }
+
+  /**
+   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+   * Beware that changing an allowance with this method brings the risk that someone may use both the old
+   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+   * @param spender The address which will spend the funds.
+   * @param value The amount of tokens to be spent.
+   */
+  function approve(address spender, uint256 value) public returns (bool) {
+    require(spender != address(0));
+
+    _allowed[msg.sender][spender] = value;
+    emit Approval(msg.sender, spender, value);
+    return true;
+  }
+
+    /**
+   * @dev Transfer tokens from one address to another
+   * @param from address The address which you want to send tokens from
+   * @param to address The address which you want to transfer to
+   * @param value uint256 the amount of tokens to be transferred
+   */
+  function transferFrom(
+    address from,
+    address to,
+    uint256 value
+  )
+    public
+    returns (bool)
+  {
+    require(value <= _balances[from]);
+    require(value <= _allowed[from][msg.sender]);
+    require(to != address(0));
+
+    _balances[from] = _balances[from] - value;
+    _balances[to] = _balances[to] + value;
+    _allowed[from][msg.sender] = _allowed[from][msg.sender] - value;
+    emit Transfer(from, to, value);
+    return true;
+  }
+
+
+  /**
+   * @dev Increase the amount of tokens that an owner allowed to a spender.
+   * approve should be called when allowed_[_spender] == 0. To increment
+   * allowed value is better to use this function to avoid 2 calls (and wait until
+   * the first transaction is mined)
+   * From MonolithDAO Token.sol
+   * @param spender The address which will spend the funds.
+   * @param addedValue The amount of tokens to increase the allowance by.
+   */
+  function increaseAllowance(
+    address spender,
+    uint256 addedValue
+  )
+    public
+    returns (bool)
+  {
+    require(spender != address(0));
+
+    _allowed[msg.sender][spender] = (
+      _allowed[msg.sender][spender] + addedValue);
+    emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+    return true;
+  }
+
+  /**
+   * @dev Decrease the amount of tokens that an owner allowed to a spender.
+   * approve should be called when allowed_[_spender] == 0. To decrement
+   * allowed value is better to use this function to avoid 2 calls (and wait until
+   * the first transaction is mined)
+   * From MonolithDAO Token.sol
+   * @param spender The address which will spend the funds.
+   * @param subtractedValue The amount of tokens to decrease the allowance by.
+   */
+  function decreaseAllowance(
+    address spender,
+    uint256 subtractedValue
+  )
+    public
+    returns (bool)
+  {
+    require(spender != address(0));
+
+    _allowed[msg.sender][spender] = (
+      _allowed[msg.sender][spender] - subtractedValue);
+    emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+    return true;
+  }
+
+  /**
+   * @dev Internal function that mints an amount of the token and assigns it to
+   * an account. This encapsulates the modification of balances such that the
+   * proper events are emitted.
+   * @param account The account that will receive the created tokens.
+   * @param amount The amount that will be created.
+   */
+  function _mint(address account, uint256 amount) internal {
+    require(account != address(0));
+    _totalSupply = _totalSupply + amount;
+    _balances[account] = _balances[account] + amount;
+    emit Transfer(address(0), account, amount);
+  }
+
+  /**
+   * @dev Internal function that burns an amount of the token of a given
+   * account.
+   * @param account The account whose tokens will be burnt.
+   * @param amount The amount that will be burnt.
+   */
+  function _burn(address account, uint256 amount) internal {
+    require(account != address(0));
+    require(amount <= _balances[account]);
+
+    _totalSupply = _totalSupply - amount;
+    _balances[account] = _balances[account] - amount;
+    emit Transfer(account, address(0), amount);
+  }
+
+  /**
+   * @dev Internal function that burns an amount of the token of a given
+   * account, deducting from the sender's allowance for said account. Uses the
+   * internal burn function.
+   * @param account The account whose tokens will be burnt.
+   * @param amount The amount that will be burnt.
+   */
+  function _burnFrom(address account, uint256 amount) internal {
+    require(amount <= _allowed[account][msg.sender]);
+
+    // Should https://github.com/OpenZeppelin/zeppelin-solidity/issues/707 be accepted,
+    // this function needs to emit an event with the updated approval.
+    _allowed[account][msg.sender] = _allowed[account][msg.sender] - amount;
+    _burn(account, amount);
   }
 
   function deposit(uint256 _amount) public {
       require(address(this).balance >= _amount, "insufficient funds to deposit");
       require(owner == msg.sender, "You may only deposit from your own account");
-      balances[msg.sender] += _amount;
+      _balances[msg.sender] += _amount;
 
       emit Deposit(_amount);
   }
 
   function withdraw(uint256 _amount) public {
     require(msg.sender == owner, "You may only withdraw from your own account");
-    require(balances[msg.sender] >= _amount, "insufficient funds in your account");
+    require(_balances[msg.sender] >= _amount, "insufficient funds in your account");
 
-    balances[msg.sender] -= _amount;
+    _balances[msg.sender] -= _amount;
 
     emit Withdraw(_amount);
   }
 
-  function balanceOf(address _account) public view returns (uint) {
-    return balances[_account];
-  }
-
   function setBalance(address _account, uint256 _amount) public {
-    require(balances[_account] == 0, "balance of the account is not zero");
+    require(_balances[_account] == 0, "balance of the account is not zero");
     // require(msg.sender == lendingAddress, "only the lending contract can do this");
-
-    balances[_account] += _amount;
-    // _mint(_account, _amount);  // if using OpenZeppelin's ERC20 implementation,
-    // simply call this funcction instead of doing balances[_account] += _amount;
+    _mint(_account, _amount);
   }
 
   function incrementBalance(address _account, uint256 _amount) public {
     // require(msg.sender == validationAddress, "only the validation contract can do this");
-    balances[_account] += _amount;
-    // _mint(_account, _amount);  // if using OpenZeppelin's ERC20 implementation,
-    // simply call this funcction instead of doing balances[_account] += _amount;
+    _mint(_account, _amount);
   }
 }
