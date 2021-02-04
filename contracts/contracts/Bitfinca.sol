f56c504b476e5d0930593a84fc9b2aba789034dc
Jaclyn Chan <jaclyn.chan@curvegrid.com>
2021-02-04 15:43:25 +0800

    add modifiers

--- contracts/contracts/Bitfinca.sol
+++ contracts/contracts/Bitfinca.sol
@@ -12,6 +12,7 @@ contract Bitfinca {
   struct Lender {
     string name;
     address account;
+    bool valid; // lender is active
   }
 
    struct Entrepreneur {
@@ -19,98 +20,119 @@ contract Bitfinca {
     address account;
     uint target;
     uint creditScore; // on a scale of 0-800
+    bool valid; // entrepreneur is active
   }
 
   struct Validator {
     string name;
     address account;
+    bool valid; // validator is active
   }
 
-  Lender[] public lenders;
-  Entrepreneur[] public entrepreneurs;
-  Validator[] public validators;
+  /* Roles  */
+  mapping(address => Lender) public lenders;
+  mapping(address => Entrepreneur) public entrepreneurs;
+  mapping(address => Validator) public validators;
+
+  /* RETRIEVING */
+  address[] public allLenders;
+  address[] public allEntrepreneurs;
+  address[] public allValidators;
 
   /* Events */
-  event NewEntrepreneur(uint256 id, string name, address account);
+  event NewEntrepreneur(string name, address account);
   event Validate(address business, bool approval);
 
   /* Mappings */
-  mapping(address => uint256) public entrepreneurAddressToID; // entrepreneur address => ID
-  mapping(address => uint256) public lenderAddressToID; // lender address => ID
-  mapping(address => uint256) public validatorAddressToID; // validator address=>ID
   mapping (address => uint256) public fundingTarget; // entrepreneur address => fundingTarget
-  mapping(address => mapping(address => bool)) public businessToValidators; // businessID => Validator => approval
-  mapping(address => uint) public businessValidatorCount; // businessID => validatorCount
-  mapping(uint => uint) public entrepreneurToCreditsScore; // id to credit score
+  mapping(address => mapping(address => bool)) public entrepreneurToValidators; // entrepreneur address => Validator => approval
+  mapping(address => uint) public entrepreneurValidatorCount; // entrepreneur address => validatorCount
+  mapping(address => uint) public entrepreneurToCreditsScore; // entrepreneur address to credit score
+
+  /* Modifiers*/
+  modifier checkValidLender(address _account) {
+    require(lenders[_account].account != address(0));
+    require(lenders[_account].valid);
+    _;
+  }
+
+  modifier checkValidEntrepreneur(address _account) {
+    require(entrepreneurs[_account].account != address(0));
+    require(entrepreneurs[_account].valid);
+    _;
+  }
+
+  modifier checkValidValidator(address _account) {
+    require(validators[_account].account != address(0));
+    require(validators[_account].valid);
+    _;
+  }
 
   function addLender(string memory _name, address _account) public {
-    require(lenderAddressToID[_account] == 0, "You have already registered to be a lender");
-    lenders.push(Lender({
+    require(lenders[_account].account != address(0), "You have already registered to be a lender");
+    lenders[_account] = Lender({
         name: _name,
-        account: _account
-    }));
-    uint id = lenders.length;
-    lenderAddressToID[_account] = id;
+        account: _account,
+        valid: true
+    });
+    allLenders.push(_account);
   }
 
   function addEntrepreneur(string memory _name, address _account, uint _target) public {
-    require(entrepreneurAddressToID[_account] == 0, "You have already registered your business");
-    entrepreneurs.push(Entrepreneur(_name,_account, _target));
-    uint id = entrepreneurs.length;
+    require(entrepreneurs[_account].account != address(0), "You have already registered your business");
+    entrepreneurs[_account] = Entrepreneur(_name,_account, _target, 690, true);
     fundingTarget[_account] = _target;
-    entrepreneurAddressToID[_account] = id;
-    businessValidatorCount[_account] = 0; // set validator count to 0
-    emit NewEntrepreneur(id, _name, _account);
+    entrepreneurValidatorCount[_account] = 0; // set validator count to 0
+    allEntrepreneurs.push(_account);
+
+    emit NewEntrepreneur(_name, _account);
   }
 
   function addValidator(string memory _name, address _account) public {
-    require(validatorAddressToID[_account] == 0, "You have already registered to be a validator");
-    validators.push(Validator({
+    require(validators[_account].account != address(0), "You have already registered to be a validator");
+    validators[_account] = Validator({
         name: _name,
-        account: _account
-    }));
-    uint id = validators.length;
-    validatorAddressToID[_account] = id;
+        account: _account,
+        valid: true
+    });
+    allValidators.push(_account);
   }
 
   /* Entrepreneur */
-  function setTarget(address _account, uint256 _target) public {
+  function setTarget(address _account, uint256 _target) public checkValidEntrepreneur(_account) {
     require(msg.sender == _account, "only the business owner can change the target");
     fundingTarget[_account] = _target;
-    entrepreneurs[entrepreneurAddressToID[_account]-1].target = _target;
+    entrepreneurs[_account].target = _target;
   }
 
-  function addToTarget(address _account, uint256 _target) public {
+  function addToTarget(address _account, uint256 _target) public checkValidEntrepreneur(_account) {
     require(msg.sender == _account, "only the business owner can change the target");
     fundingTarget[_account] += _target;
-    entrepreneurs[entrepreneurAddressToID[_account]-1].target += _target;
+    entrepreneurs[_account].target += _target;
   }
   /* Entrepreneur end */
 
   /* Validators */
-  function validate(address business, bool approval) public {
-    require(validatorAddressToID[msg.sender] != 0, "validator must be registered"); // validator must be registered
+  function validate(address business, bool approval) public checkValidValidator(msg.sender) {
     require(msg.sender != business, "validator cannot validate it's own business"); // validator != entrepreneur
 
-    uint entrepreneurID = entrepreneurAddressToID[business];
-
     if (approval) {
-        businessValidatorCount[business] += 1;
-        if (entrepreneurToCreditsScore[entrepreneurID] == 0) {
-          entrepreneurToCreditsScore[entrepreneurID] = 690;
+        entrepreneurValidatorCount[business] += 1;
+        if (entrepreneurToCreditsScore[business] == 0) {
+          entrepreneurToCreditsScore[business] = 690;
         } else {
-          entrepreneurToCreditsScore[entrepreneurID] = entrepreneurToCreditsScore[entrepreneurID] + 50;
+          entrepreneurToCreditsScore[business] = entrepreneurToCreditsScore[business] + 50;
         }
     }
     else {
-        businessValidatorCount[business] -= 1;
-        if (entrepreneurToCreditsScore[entrepreneurID] == 0) {
-          entrepreneurToCreditsScore[entrepreneurID] = 400;
+        entrepreneurValidatorCount[business] -= 1;
+        if (entrepreneurToCreditsScore[business] == 0) {
+          entrepreneurToCreditsScore[business] = 400;
         } else {
-          entrepreneurToCreditsScore[entrepreneurID] = entrepreneurToCreditsScore[entrepreneurID] - 50;
+          entrepreneurToCreditsScore[business] = entrepreneurToCreditsScore[business] - 50;
         }
     }
-    businessToValidators[business][msg.sender] = approval;
+    entrepreneurToValidators[business][msg.sender] = approval;
 
     Token(accountAddress).incrementBalance(msg.sender, 1); // placeholder for the nominal fee validators earn
 
